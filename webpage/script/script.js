56543072d94ba37b6b421d01dddf28afc0fb07f5
Oscar <osro2011@edu.laroverken.se>
2021-12-03 14:32:40 +0100

    fixed hover

--- webpage/script/script.js
+++ webpage/script/script.js
@@ -1,5 +1,22 @@
+function onMouseEnter(target) {
+    for (path of document.getElementsByClassName(target)) {
+        path.style.fill = "#808080"
+    }
+}
+
+function onMouseLeave(target) {
+    console.log("left")
+    for (path of document.getElementsByClassName(target)) {
+        path.style.fill= "#b3b3b3"
+    }
+}
+
 function onClickSE() {
     alert("Sweden clicked.")
+}
+
+function onHoverSE() {
+    document.getElementById("SE").style.fill = "#fff"
 }
 
 function onClickRU() {
